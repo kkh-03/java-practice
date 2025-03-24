cd5527e0a2d26a3837016e4cbc96dd989b52a78c
kkh <kkh99480@naver.com>
2025-03-24 14:29:16 +0900

    4주차 원의 면적 구하기, 이름과 나이 출력

---
+++ 2025.03.24/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+
+        String name = "김근형";
+        int age = 23;
+        System.out.println(name + "님은 " + age + "살입니다");
+    }
+}
